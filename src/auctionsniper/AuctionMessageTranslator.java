3abf982ccc2ed8e827183df40f314078a9a304e8
Gesa Stupperich <gesa.stupperich@gmail.com>
2020-12-31 11:14:04 +0000

    Tidy up message translator by extracting AuctionEvent value object

--- src/auctionsniper/AuctionMessageTranslator.java
+++ src/auctionsniper/AuctionMessageTranslator.java
@@ -15,23 +15,44 @@ public class AuctionMessageTranslator implements MessageListener {
 
     @Override
     public void processMessage(Chat chat, Message message) {
-        HashMap<String, String> event = unpackEventFrom(message);
+        AuctionEvent event = AuctionEvent.from(message.getBody());
 
-        String type = event.get("Event");
+        String type = event.type();
         if ("CLOSE".equals(type)) {
             listener.auctionClosed();
         } else if ("PRICE".equals(type)) {
-            listener.currentPrice(Integer.parseInt(event.get("CurrentPrice")),
-                                  Integer.parseInt(event.get("Increment")));
+            listener.currentPrice(event.currentPrice(), event.increment());
         }
     }
 
-    private HashMap<String, String> unpackEventFrom(Message message) {
-        HashMap<String, String> event = new HashMap<>();
-        for (String element: message.getBody().split(";")) {
-            String[] pair = element.split(":");
-            event.put(pair[0].trim(), pair[1].trim());
+    private static class AuctionEvent {
+        private HashMap<String, String> fields = new HashMap<>();
+        public String type() {
+            return fields.get("Event");
         }
-        return event;
+        public int currentPrice() {
+            return Integer.parseInt(fields.get("CurrentPrice"));
+        }
+        public int increment() {
+            return Integer.parseInt(fields.get("Increment"));
+        }
+
+        static String[] fieldsIn(String body) {
+            return body.split(";");
+        }
+
+        static AuctionEvent from(String messageBody) {
+            AuctionEvent event = new AuctionEvent();
+            for (String field: fieldsIn(messageBody)) {
+                event.addField(field);
+            }
+            return event;
+
+        }
+
+        private void addField(String field) {
+            String[] pair = field.split(":");
+            fields.put(pair[0].trim(), pair[1].trim());
+        }
     }
 }
